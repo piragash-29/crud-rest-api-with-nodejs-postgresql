3af759b734998e3f0261c4a858cb8632fabc28c3
Piragash <piragash.vi@gmail.com>
2023-05-05 23:12:42 +0530

    config postgresql

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+const db = require('./src/queries')
+
 app.use(bodyParser.json())
 app.use(
     bodyParser.urlencoded({
@@ -17,3 +19,9 @@ app.get('/', (request, response) => {
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
 })
+
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
